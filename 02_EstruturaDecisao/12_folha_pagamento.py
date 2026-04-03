2ebb16cd0d1ee3511b6b09005ae253c0aa501428
AlexGsouza <alexg_souza@hotmail.com>
2026-04-03 12:15:39 -0300

    Atualizaçãode codigo folha pagamento

--- 02_EstruturaDecisao/12_folha_pagamento.py
+++ 02_EstruturaDecisao/12_folha_pagamento.py
@@ -1,15 +1,13 @@
 # Folha de pagamento
-
 valorPorHora = float(input("Digite o valor por hora: "))
-quantHoras =\
-    float(input("Digite a quantidade de horas trabalhadas no mês: "))
+quantHoras = float(input("Digite a quantidade de horas trabalhadas no mês: "))
 
 # Cálculo do salário bruto
 salarioBruto = valorPorHora * quantHoras
 
 #calculo do imposto de renda
 if salarioBruto > 2500:
-    aliquotaIR = 20
+    aliquotaIR = 7.9190
 elif salarioBruto > 1500:   
     aliquotaIR = 10
 elif salarioBruto > 900:
@@ -19,17 +17,28 @@ else:
 
 valorIR = salarioBruto * (aliquotaIR / 100)
 
+#adiantamento
+vale = salarioBruto * 0.45
+
 # calculo do sindicato
 valorSindicato = salarioBruto * (3 /100.0)
 
 # calculo do Descontos
-totalDescontos = valorIR + valorSindicato
+totalDescontos = valorIR + valorSindicato + vale
+
 
 # cálculo valor FGTS
 valorFGTS = salarioBruto * (11 / 100.0)
 
 # cálculo do salário líquido
 salarioLiquido = salarioBruto - totalDescontos
+
+print(f"+ Salário Bruto: R$ {salarioBruto:.2f}")
+print(f"- Vale: R$ {vale:.2f}")
+print(f"- IR: R$ {valorIR:.2f}")
+print(f"- Sindicato: R$ {valorSindicato:.2f}")
+print(f"= Salário Liquido: R$ {salarioLiquido:.2f}")
+
 
 
 
